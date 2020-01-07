64c492f51bdf0ad452ef1bc9cd45d7ddefdafb0f
Igor Khotin <khotin@gmx.com>
2020-01-07 14:20:33 +0200

    [+] console focus capture and release

--- console.mod/trap/f4Down.js
+++ console.mod/trap/f4Down.js
@@ -2,5 +2,11 @@ let shots = 0
 
 module.exports = function(e) {
     if (e.repeat) return
-    _.lab.hud.console.hidden = !_.lab.hud.console.hidden
+
+    if (lab.hud.console.hidden) {
+        lab.hud.captureFocus(lab.hud.console)
+    } else {
+        lab.hud.releaseFocus(lab.hud.console)
+    }
+    lab.hud.console.hidden = !_.lab.hud.console.hidden
 }
