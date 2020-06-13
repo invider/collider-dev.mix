194de507f3f6c4f28923d756fd23edfdfa3e1036
Igor Khotin <khotin@gmx.com>
2020-06-13 22:36:30 +0300

    [+] select console command

---
+++ console.mod/cmd/select.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function select(args, line, con) {
+    const path = args[1]
+    if(!sys.isString(path)) {
+        con.print('expecting <path> argument')
+        return
+    }
+    const nodes = _$.select(path)
+
+    nodes.forEach(node => {
+        log.dump(node)
+    })
+}
+
+select.info = 'select and dump nodes by path'
+select.args = '<path>'
+
+module.exports = select
