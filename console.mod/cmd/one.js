a3d3ede813a128388a50555a0b9c205e1ada5353
Igor Khotin <khotin@gmx.com>
2020-06-13 22:36:14 +0300

    [+] one console command

---
+++ console.mod/cmd/one.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function one(args, line, con) {
+    const path = args[1]
+    if(!sys.isString(path)) {
+        con.print('expecting <path> argument')
+        return
+    }
+    const node = _$.selectOne(path)
+
+    log.dump(node)
+}
+
+one.info = 'select and dump a single node form provided path'
+one.args = '<path>'
+
+module.exports = one
