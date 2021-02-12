a3131018e56d03a4a9f66241039ac801a3237b4c
Igor Khotin <khotin@gmx.com>
2021-02-12 20:26:37 +0200

    [*] disable other mods on console activation

--- console.mod/trap/f4Down.js
+++ console.mod/trap/f4Down.js
@@ -2,8 +2,10 @@ module.exports = function(e) {
     if (e && e.repeat) return
 
     if (lab.hud.console.hidden) {
+        _.disableOthers()
         lab.hud.captureFocus(lab.hud.console)
     } else {
+        _.enableOthers()
         lab.hud.releaseFocus(lab.hud.console)
     }
     lab.hud.console.hidden = !_.lab.hud.console.hidden
